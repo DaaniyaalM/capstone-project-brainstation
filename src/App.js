c8f4fa66bd1ffee47613e428161c2b71ab57f0b7
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-22 10:29:24 -0800

    got working music player using music from JSON, now will update with possible stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as Tone from "tone";
+import music from "./data/mario.json";
 
-function App() {
+console.log(music.tracks[0].notes);
+
+const MusicPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const MainArray = [
+    ["0", "Bb4"],
+    ["0:2", "Bb4"],
+    ["0:4", "Fb3"],
+    ["0:5", "Bb4"],
+    ["0:6", "A4"],
+    ["1:0", "G4"],
+    ["1:2", "A4"],
+    // "A2:2n",
+    // "A2:2n",
+    // "G2:4n",
+    // "F#2:4n",
+    // "G2:4n",
+    // "G2:4n",
+    // "G2:4n",
+    // "G#2:4n",
+    // "G#2:4n",
+    // "G2:2n",
+    // "D#2:2n",
+    // "D#2:2n",
+    // "G2:4n",
+    // "A2:4n",
+  ];
+
+  const playMusic = () => {
+    const synth = new Tone.Synth().toDestination();
+    const part = new Tone.Part((time, note) => {
+      synth.triggerAttackRelease(note.name, note.duration, note.time);
+    }, music.tracks[0].notes).start(); // <-- Change notesArray to MainArray
+    // if (!isPlaying) {
+
+    // const osc = new Tone.Oscillator(440, "sine").toDestination().start();
+    Tone.Transport.start();
+    setIsPlaying(true);
+
+    // } else {
+    //   Tone.Transport.stop();
+    //   Tone.Transport.cancel();
+    //   setIsPlaying(false);
+    // }
+
+    if (Tone.context.state !== "running") {
+      Tone.context.resume();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={playMusic}>
+        {isPlaying ? "Stop Music" : "Play Music"}
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+export default MusicPlayer;
